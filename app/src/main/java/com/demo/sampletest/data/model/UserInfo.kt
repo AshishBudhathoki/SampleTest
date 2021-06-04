f54e897dde42ce49564c12258d8b4972ffede2df
ashish <ashish.budhathoki@outlook.com>
2021-06-05 07:22:38 +1000

    Add data models

---
+++ app/src/main/java/com/demo/sampletest/data/model/UserInfo.kt
@@ -0,0 +1,8 @@
+package com.demo.sampletest.data.model
+
+data class UserInfo(
+    val id: Int,
+    val username: String,
+    val email: String,
+    val phone: String
+)
---
+++ app/src/main/java/com/demo/sampletest/data/model/UserPhotos.kt
@@ -0,0 +1,9 @@
+package com.demo.sampletest.data.model
+
+data class UserPhotos(
+    val albumId: Int,
+    val id: Int,
+    val title: String,
+    val url: String,
+    val thumbnailUrl: String
+)
